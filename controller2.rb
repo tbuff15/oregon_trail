0de82ef3c32be8985c20acde02930a83063687eb
Henry Faversham <henryfaversham@Henrys-MacBook-Air.local>
2016-04-25 13:44:24 -0500

    Updated

---
+++ controller2.rb
@@ -0,0 +1,27 @@
+require 'sinatra'
+
+get '/' do
+	erb :index	
+end
+
+post '/mountain_river' do 
+	if params[:mountain_river] = river
+				@river = "/river"
+				erb :general
+			elsif 
+				@mountain = "/mountain"
+				erb :general
+			end
+end
+
+post 'river' do 
+	if params[:river] = raft
+		@raft = "/raft"
+		erb :general
+		elsif 
+			@ride_through = '/ride_through'
+			erb :general
+		end
+end
+
+
