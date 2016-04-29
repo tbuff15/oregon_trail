c2a4d60c49b91e935219b8193dd80a4a6285f384
Henry Faversham <henryfaversham@Henrys-MacBook-Air.local>
2016-04-29 10:32:41 -0500

    hi

--- controller_cyoa.rb
+++ controller_cyoa.rb
@@ -13,20 +13,20 @@ get '/direction/:choice' do
 	when "river"
 		@headline ="You reach the river"
 		@question = "You can go on through or build a raft"
-		@option_a = "go through"
+		@option_a = "Go through"
 		@option_b = "build a raft" 
 		erb :index
-			when "build a raft"
+			when "build"
 				@headline = "You're children drown and you're wagon capsizes."
 				@question = "You can take your chances and search for food, or you can eat your last companion."
 				@option_a = "Take my chances."
 				@option_b = "Eat my companion."
 				erb :index
-					when "Take my chances."
+					when "Take"
 						@headline = "You starve to death."
 						@question = "Too bad. So sad." 
 						erb :ending
-					when "Eat my companion."
+					when "Eat"
 						@headline = "You ate your last companion, but it only lasted you three days."
 						@question = "You died cold, alone, and a cannibal. Congrats."
 						erb :ending 
@@ -43,15 +43,15 @@ get '/direction/:choice' do
 				@question = "You were caught in an avalanche and you died, cold and alone."
 				erb :ending 
 
-			when "SLOW" || "go through"
+			when "SLOW" || "Go"
 				@headline = "You made it through, but you're low on food..."
 				@question = "You come across some bison. You can..."
-				@option_a = "Go around."
+				@option_a = "Ignore the bison."
 				@option_b = "Try to shoot for food."
 				erb :index 
-					when "Try to shoot for food."
+					when "Try"
 						@headline = "You miss your first shot."
-						@qustion= "You can shoot again or run away."
+						@qustion = "You can shoot again or run away."
 						@option_a = "Shoot again"
 						@option_b = "Run away"
 						erb :index 
@@ -59,27 +59,27 @@ get '/direction/:choice' do
 								@headline = "You spooked them. They stampeded."
 								@question = "You died."
 								erb :ending 
-					when "Run away" || "Go around."
+					when "Run" || "Ignore"
 						@headline = "You come across a small town and you have no more food, but you don't have money either. "
 						@question = "The food is too expensive. You can keep going and search for cheaper food or you can steal some."
 						@option_a = "Pass by without food"
 						@option_b = "Steal food"
 						erb :index 
-							when "Steal food"
+							when "Steal"
 								@headline = "You are shot by other bandits trying to steal the same food."
 								@question = "Sorry. Karma."
 								erb :ending
-							when "Pass by without food"
+							when "Pass"
 								@headline = "You find another town with free food and supplies."
 								@question = "You make it to Oregon! Congrats. "
-								@option_a = "You can stay put and build a life."
+								@option_a = "you can stay put and build a life."
 								@option_b = "You take a chance to find gold."
 								erb :index 
-									when "You can stay put and build a life."
+									when "you"
 										@headline = "You live the humble life of a settler."
 										@question = "YAY. You survived the Oregon trail."
 										erb :ending 
-									when "You take a chance to find gold."
+									when "You"
 										@headline = "You struck gold!!"
 										@question = "You die rich. You conquered the Oregon trail."
 										erb :ending 
